4c20049de9ff0a370a74eece53edab023e72e0a2
Saurabh Madhu Singh <saurabhsingh091196@gmail.com>
2022-04-05 21:43:33 -0700

    Created app.py for s3

---
+++ s3/v1/app.py
@@ -0,0 +1,89 @@
+"""
+SFU CMPT 756
+Sample application---Playist service.
+"""
+
+# Standard library modules
+import logging
+import os
+import sys
+
+# Installed packages
+from flask import Blueprint
+from flask import Flask
+from flask import request
+from flask import Response
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import requests
+
+import simplejson as json
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Playlist process')
+
+db = {
+    "name": "http://cmpt756db:30002/api/v1/datastore",
+    "endpoint": [
+        "read",
+        "write",
+        "delete",
+        "update"
+    ]
+}
+bp = Blueprint('app', __name__)
+
+
+@bp.route('/health')
+@metrics.do_not_track()
+def health():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/readiness')
+@metrics.do_not_track()
+def readiness():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/', methods=['GET'])
+@metrics.do_not_track()
+def list_all():
+    headers = request.headers
+    return "API is working. Please access the required URL"
+
+
+@bp.route('/<playlist_id>', methods=['GET'])
+def get_playlist(playlist_id):
+    headers = request.headers
+    #check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    response_json = response.json()
+    #TODO
+    #add handling if response isnt found
+    return (response_json)
+
+# All database calls will have this prefix.  Prometheus metric
+# calls will not---they will have route '/metrics'.  This is
+# the conventional organization.
+app.register_blueprint(bp, url_prefix='/api/v1/playlists/')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        logging.error("missing port arg 1")
+        sys.exit(-1)
+    p = int(sys.argv[1])
+    # Do not set debug=True---that will disable the Prometheus metrics
+    app.run(host='0.0.0.0', port=p, threaded=True)
